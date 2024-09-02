b582fa126d333cc6fc95b40edb2298a663a74e6d
BAECHAN <54573684+BAECHAN@users.noreply.github.com>
2024-09-02 17:54:42 +0900

    Update common.d.ts

--- common.d.ts
+++ common.d.ts
@@ -113,6 +113,7 @@ interface SearchSortPlan {
 interface Plan {
   planId: number;
   title: string;
+  visibilityType: 'public' | 'private';     // private 은 mine에만, public은 explore에 노출됨
   likeCount: number;
   author: string;   // my plan은 이게 내꺼인거만
   createdDate: Date;
