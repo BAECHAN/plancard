f4276b3e314f1eb3fa1bd3a97abcf8faf0cdb005
baechan <asdfjklddd@naver.com>
2024-09-08 12:38:54 +0900

    검색을 위한 타입 수정 중

--- common.d.ts
+++ common.d.ts
@@ -9,6 +9,8 @@ interface ApiResponse<T> {
 
 type DateToString = 'yyyy-MM-dd';
 type PlanVisibility = 'public' | 'private'; // private 은 mine에만, public은 explore에 노출됨 - 여기서 선택된 타입은 DailyPlan에도 일괄 적용됨 
+type CountryCode = 'US' | 'KR' | 'JP' | 'CN' | 'FR';
+type SortOrder = 'asc' | 'desc'
 
 interface PaginationResponse extends PaginationRequest{
   totalPages: number,
@@ -30,93 +32,65 @@ interface Card {
   category: Category;
   rating: number; // 1 ~ 5
   googleMapLink: string;
-  imageUrlList: CardImage[];
+  imageList: CardImage[];
   scrap: boolean;
-}
-
-type CardTag = {
-  tagId: number;
-  tagName: string;
-};
-
-interface CardImage {
-    imageId: number;
-    imageUrl: string;
-    alt: string;
-    isMain: boolean;
 }
 
 interface MyCard extends Card {
   getDate: Date;
   lastUpdateDate: Date;
-  myImageUrlList: CardImage[];
+  myImageList: CardImage[];
   myMemo: string;
   myTagList: CardTag[];
 }
 
-interface RegionBase {
-  title: string;         // 국가 이름
-  description: string;
-  imageUrl: string; // 대표 이미지
-}
-
-interface Country extends RegionBase {
-  countryId: number;
-  isoCode: CountryCode; // ISO 3166-1 alpha-2 코드 (예: "US", "KR")
-}
-
-interface City extends RegionBase{
-  cityId: number;
-  countryIsoCode: Country['isoCode']; // ISO 국가 코드 (Country와 연결)
-}
-
-type CountryCode = 'US' | 'KR' | 'JP' | 'CN' | 'FR';
-
-type Category = {
-  categoryId: number;
-  title: 'attraction' | 'restaurant' | 'cafe' | 'mart' | 'transportation' | 'lodging' | 'others'; // 관광지, 음식점, 카페, 마트, 교통수단, 숙소, 기타
-}
-
-type Theme = {
-  themeId: number;
-  title: string; // 해변, 도시, 고대, 산
-}; 
-
-interface SearchCardForm {
-  type: 'owned' | 'all';
+type SearchBaseForm = {
   pagination: PaginationRequest;
-  
+  sort: SearchSortBase;
+}
+
+interface SearchRegionForm extends SearchBaseForm {
+  filter?: SearchFilterBase;
+}
+
+interface SearchCardForm extends SearchBaseForm {
+  sort: SearchSortCard;
   search?: string;
   filter?: SearchFilterCard;
-  sort?: SearchSortCard;
 }
 
-interface SearchPlanForm {
-  search: string;
-  type: 'owned' | 'all';
-  filter: SearchFilterPlan;
+interface SearchPlanForm extends SearchBaseForm {
   sort: SearchSortPlan;
+  search?: string;
+  filter?: SearchFilterPlan;
 }
-type SearchFilterCard = {
-  scrap?: boolean,
+
+
+type SearchFilterBase = {
+  scrap: boolean,
   country?: Country['isoCode'],
   city?: City['title'],
   theme?: Theme[],
   category?: Category[],
 }
 
-interface SearchSortCard {
+type SearchFilterCard = SearchFilterBase;
+
+interface SearchFilterPlan extends SearchFilterBase {
+  like: boolean,
+}
+
+interface SearchSortBase {
+  sortBy: string;
+  sortOrder: SortOrder
+}
+
+interface SearchSortCard extends SearchSortBase {
   sortBy: 'cardTitle' | 'cityTitle' | 'rating' | 'lastUpdateDate' | 'getDate'
-  sortOrder: 'asc' | 'desc'
 }
 
-type SearchFilterPlan = {
-
-}
-
-interface SearchSortPlan {
+interface SearchSortPlan extends SearchSortBase {
   sortBy: 'planName' | 'createdDate' | 'lastUpdateDate' | 'likeCount'
-  sortOrder: 'asc' | 'desc'
 }
 
 interface Plan {
@@ -137,18 +111,13 @@ interface Plan {
   startDate: DateToString;  // 여행 시작 일자
   endDate: DateToString;    // 여행 끝 일자
   duration: number; // 여행 끝 일자 - 여행 시작 일자 + 1
-
   tagList: CardTag[]; // country, city, theme, category 태그
 }
+
 
 interface PlanWithDay {
   planId: number;
   dayList: Day[]
-}
-
-interface PlanWithMemo {
-  planId: number;
-  memo: string;
 }
 
 interface Day {
@@ -170,6 +139,12 @@ interface Day {
   bridgeList: Bridge[];
 }
 
+interface PlanWithMemo {
+  planId: number;
+  memo: string;
+}
+
+
 interface Bridge {
   id: number;
   memo: string;
@@ -180,3 +155,40 @@ interface User {
   nickname: string;
 }
 
+interface RegionBase {
+  title: string;         // 국가 이름
+  description: string;
+  imageUrl: string; // 대표 이미지
+}
+
+interface Country extends RegionBase {
+  countryId: number;
+  isoCode: CountryCode; // ISO 3166-1 alpha-2 코드 (예: "US", "KR")
+}
+
+interface City extends RegionBase{
+  cityId: number;
+  countryIsoCode: Country['isoCode']; // ISO 국가 코드 (Country와 연결)
+}
+
+type Category = {
+  categoryId: number;
+  title: 'attraction' | 'restaurant' | 'cafe' | 'mart' | 'transportation' | 'lodging' | 'others'; // 관광지, 음식점, 카페, 마트, 교통수단, 숙소, 기타
+}
+
+type Theme = {
+  themeId: number;
+  title: string; // 해변, 도시, 고대, 산
+}; 
+
+type CardTag = {
+  tagId: number;
+  tagName: string;
+};
+
+interface CardImage {
+    imageId: number;
+    imageUrl: string;
+    alt: string;
+    isMain: boolean;
+}
